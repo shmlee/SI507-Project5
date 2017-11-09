a67b8903bece3e0de451176109c35e099589ea4a
Sharon <shmlee@umich.edu>
2017-11-09 18:59:02 -0500

    first push

--- SI507project5_tests.py
+++ SI507project5_tests.py
@@ -1,6 +1,16 @@
 import unittest
 from SI507project5_code import *
 
+class Part1(unittest.TestCase):
+	def setUp(self):
+		self.followers = open('followers.csv', 'r')
+		self.chocolate = open('chocolate.csv','r')
+	def read_files(self):
+		self.assertTrue(self.followers.read())
+		self.assertTrue(self.chocolate.read())
+	def tearDown(self):
+		self.followers.close()
+		self.chocolate.close()
 
 
 
